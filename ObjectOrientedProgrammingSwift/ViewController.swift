edd53fddf2410ab1bc4c1824cc94487bd8c25050
Fauzi <fauzibinfaisal@gmail.com>
2019-05-15 13:21:31 +0700

    Initial Commit

---
+++ ObjectOrientedProgrammingSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ObjectOrientedProgrammingSwift
+//
+//  Created by Fauzi Fauzi on 15/05/19.
+//  Copyright © 2019 Fauzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
